2fa68c7b07024419a2aeb564b14dc91a976bbf21
Ramiza1998 <ramizayakubu994@gmail.com>
2022-03-16 08:41:31 -0700

    first commit

---
+++ src/writers.js
@@ -0,0 +1,42 @@
+const writers=[
+    {
+        id:1,
+        name: "jamila lorde",
+        avatar: "audre_lorde",
+        email:"jamila23@gmail.com",
+        pnone:"0546282732" ,
+
+    },
+    {
+        id:2,
+name: "jamila lorde",
+avatar: "belle_hooks",
+email:"jamila23@gmail.com",
+pnone:"0546282732" ,
+    },
+    {
+        id:3,
+name: "jamila lorde",
+avatar: "du_bois",
+email:"jamila23@gmail.com",
+pnone:"0546282732" ,
+    },
+    {
+        id:4,
+        name: "jamila lorde",
+        avatar: "james_baldwin",
+        email:"jamila23@gmail.com",
+        pnone:"0546282732",
+    },
+    {
+    
+        id:3,
+        name: "jamila lorde",
+        avatar: "maya_angelou",
+        email:"jamila23@gmail.com",
+        pnone:"0546282732" ,
+
+    }
+
+] 
+export default writers;
